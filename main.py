5625692aac2bea6a8d2cb8e1923155383e324d6c
Henry <xuehaozhong@gmail.com>
2023-12-11 23:03:52 -0500

    improve the code

--- main.py
+++ main.py
@@ -1,44 +1,33 @@
-
 from wxauto import *
 import time
 import pyautogui
-import pyperclip
  
 msg='1'
-# 获取当前微信客户端
 wx = WeChat()
-user = 'A新人通知'
-# 获取会话列表
 wx.GetSessionList()
  
-def reply_message_if_send_key_word(user, key_words, reply_message):
-    #wx.LoadRecentMessage(1)
+def reply_message_if_send_key_word(key_words, reply_message):
     msgs = wx.GetAllMessage
     message_value = [msg[1] for msg in msgs]
     for index, msg in enumerate(msgs):
         if msg[0] in key_words:
-            
             print("*"*20)
             if reply_message not in message_value[index:]:
-                print(f"将自动回复以下语句:{reply_message}")
-                
-                
+                print(f"auto reply following msg:{reply_message}")            
                 def send():
                     pyautogui.hotkey('ctrl', 'v')
                     pyautogui.press('enter')
- 
-                def send_msg(friend):
-                    for i in range(1):
-                        send()
-                        time.sleep(0.001)
+                def send_msg():
+                    send()
+                    time.sleep(0.001)
                 if __name__ == '__main__':
-                    send_msg(user)
+                    send_msg()
             else:
                 pass
             print("*"*20)
  
 if __name__ == '__main__':
-    user = 'A新人通知'
+    user = '新人通知'
     key_word1 = 'vivan'
     key_word2 = 'sandy'
     key_word3 = 'jennie'
@@ -50,5 +39,5 @@ if __name__ == '__main__':
     reply_message = '1'
  
     while True:
-        reply_message_if_send_key_word(user=user, key_words=key_words ,reply_message=reply_message)
+        reply_message_if_send_key_word(key_words=key_words ,reply_message=reply_message)
         time.sleep(0.001)
